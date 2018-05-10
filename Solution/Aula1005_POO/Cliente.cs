91ffee2be46ec99ade33f7bb9df5c411ab18c00d
Aluno <Aluno@DOSO125275>
2018-05-10 10:33:57 -0300

    Migrando info da Classe Cliente para Pessoa

--- Solution/Aula1005_POO/Cliente.cs
+++ Solution/Aula1005_POO/Cliente.cs
@@ -1,20 +1,8 @@
 ﻿namespace Aula1005_POO {
     class Cliente {
 
-        public string Nome { get; set; }
-        public string Cpf { get; set; }
+       
 
-        public Cliente() {
-
-
-        }
-        
-
-        public Cliente(string nome, string cpf) {
-
-            this.Cpf = Cpf;
-            this.Nome = nome;
-        }
 
     }
 }
--- Solution/Aula1005_POO/Pessoa.cs
+++ Solution/Aula1005_POO/Pessoa.cs
@@ -1,6 +1,20 @@
 ﻿namespace Aula1005_POO {
     class Pessoa {
 
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+
+
+        public Pessoa() {
+
+
+        }
+
+        public Pessoa(string nome, string cpf) {
+
+            this.Cpf = Cpf;
+            this.Nome = nome;
+        }
 
     }
 }
